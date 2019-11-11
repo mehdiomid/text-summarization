7b80d8dafe7cb18b811ed23ab0a214868853a1bf
mehdiomid <omidghane@gmail.com>
2019-11-10 23:06:17 -0700

    adding the review summarization code

---
+++ review_summarization/TextUtils/textCleaner.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 15:20:54 2019
+
+@author: Mehdi
+"""
+from WordsMapping import words_mapping
+import re           
+from bs4 import BeautifulSoup 
+from nltk.corpus import stopwords   
+
+stop_words = set(stopwords.words('english')) 
+contraction_mapping = words_mapping.contraction_mapping
+
+def text_cleaner(text):
+    newString = text.lower()
+    newString = BeautifulSoup(newString, "lxml").text
+    newString = re.sub(r'\([^)]*\)', '', newString)
+    newString = re.sub('"','', newString)
+    newString = ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in newString.split(" ")])    
+    newString = re.sub(r"'s\b","",newString)
+    newString = re.sub("[^a-zA-Z]", " ", newString) 
+    tokens = [w for w in newString.split() if not w in stop_words]
+    long_words=[]
+    for i in tokens:
+        if len(i)>=3:                  #removing short word
+            long_words.append(i)   
+    return (" ".join(long_words)).strip()
+
+def summary_cleaner(text):
+    newString = re.sub('"','', text)
+    newString = ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in newString.split(" ")])    
+    newString = re.sub(r"'s\b","",newString)
+    newString = re.sub("[^a-zA-Z]", " ", newString)
+    newString = newString.lower()
+    tokens=newString.split()
+    newString=''
+    for i in tokens:
+        if len(i)>1:                                 
+            newString=newString+i+' '  
+    return newString
+
